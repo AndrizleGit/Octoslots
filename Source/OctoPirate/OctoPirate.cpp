198ab78bd05ddebbd12b08100a468fa2748043fe
AndrizleGit <167190489+AndrizleGit@users.noreply.github.com>
2026-04-14 00:43:12 +0200

    ini commit v5

---
+++ Source/OctoPirate/OctoPirate.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OctoPirate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OctoPirate, "OctoPirate" );
+
+DEFINE_LOG_CATEGORY(LogOctoPirate)
+ 
