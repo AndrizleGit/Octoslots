7f36ed9b4b1291d0e4970b6b08d16feaad0b3d30
Robin <robin_beddig@yahoo.com>
2026-06-23 18:37:03 +0200

    Fixed max health meta progression error.

--- Source/OctoPirate/Private/Character/BaseCharacter.cpp
+++ Source/OctoPirate/Private/Character/BaseCharacter.cpp
@@ -30,6 +30,11 @@ void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (BasicAttributes)
+	{
+		BasicAttributes->SetHealth(BasicAttributes->GetMaxHealth());
+	}
+
 	AttackDamage = GetAttackDamage();
 	
 	
