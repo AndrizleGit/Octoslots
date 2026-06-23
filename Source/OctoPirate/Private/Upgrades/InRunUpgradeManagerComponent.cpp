9e3ec7234f3e91abd8ed0a3fe8d02499d9d6d704
Robin <robin_beddig@yahoo.com>
2026-06-23 04:06:13 +0200

    Added max joker amount for players (current value 2)

--- Source/OctoPirate/Private/Upgrades/InRunUpgradeManagerComponent.cpp
+++ Source/OctoPirate/Private/Upgrades/InRunUpgradeManagerComponent.cpp
@@ -49,8 +49,9 @@ void UInRunUpgradeManagerComponent::CheckForLevelUp()
     bIsProcessingLevelUp = false;
 
     const bool bIsJokerLevel = (JokerLevelInterval > 0) && (CurrentLevel % JokerLevelInterval == 0);
+    const bool bCanGetJoker = AcquiredJokers.Num() < MaxJokers && AllPossibleJokers.Num() > 0;
 
-    if (bIsJokerLevel && AllPossibleJokers.Num() > 0)
+    if (bIsJokerLevel && bCanGetJoker)
     {
         RollNewJokerChoices();
         if (CurrentJokerChoices.Num() > 0)
