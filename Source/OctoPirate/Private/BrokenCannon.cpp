c2ec25919954e1dc505e2e73c8678f59ab7708f1
Tom2182 <ZTom2182Z@Gmail.com>
2026-08-03 10:05:32 +0200

    Added Coin Rewards for Treasure
    + Assigned Treasure drop to enemies that is of ABaseEnemy
    +MMC_RandCoin , 15-30 coins reward for treasure
    
    Discarded Treasure and Treasuremap Duplicated Blueprints

--- Source/OctoPirate/Private/BrokenCannon.cpp
+++ Source/OctoPirate/Private/BrokenCannon.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "UObject/ConstructorHelpers.h"
 #include "BrokenCannon.h"
+#include "UObject/ConstructorHelpers.h"
+
+
 
 
 
