f74090dfcf8553d10988d3b02b533f07a156f4ee
karo <kaharenbrock@gmail.com>
2026-08-13 14:10:10 +0200

    meta screen save wip
    - chips save pos
    - widget text wip

--- Source/OctoPirate/Public/Upgrades/UpgradeManagerComponent.h
+++ Source/OctoPirate/Public/Upgrades/UpgradeManagerComponent.h
@@ -87,7 +87,7 @@ public:
 	void RemoveUpgrade(UUpgradeData* Upgrade, int32 Levels);
 	void ApplyStatChange(EUpgradeStat Stat, float Value);
 
-	UPROPERTY()
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Upgrades")
 	TMap<UUpgradeData*, int32> UpgradeLevels;
 
 	UPROPERTY()
